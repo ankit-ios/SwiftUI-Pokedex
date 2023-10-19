d55b718fab79f7c1263964ba9b3c0007c6c01bff
Ankit Sharma <ankitsoftengi@gmail.com>
2023-10-19 20:24:11 +0530

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ankit Sharma on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
